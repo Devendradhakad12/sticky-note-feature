41555d1fa10c6b901837db9ca849b40f082bfe0d
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-12-15 22:07:42 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Home() {
+  const [color, setColor] = useState([]);
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+     
+    </div>
+  );
+}
