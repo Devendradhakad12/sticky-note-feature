8952036ecd9a92f4c29ec59bf4aac078bd60183d
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-12-15 22:39:55 +0530

    templates and open state,and Skticky Note UI created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,32 @@
 "use client";
 
 import { useState } from "react";
+import { StickyNote } from "lucide-react";
+ 
 
 export default function Home() {
-  const [color, setColor] = useState([]);
+  const [templates, setTemplates] = useState(["slate", "emerald", "red","fuchsia"]);
+  const [open, setOpen] = useState(false);
+
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <div className="flex min-h-screen flex-col p-24">
+      <div>
+        <button onClick={() => setOpen((op) => !op)}>
+          <StickyNote />
+        </button>
+        {open && 
+        <div className="ml-7">
+        {
+          templates.map((item)=>(
+            <button  className={`w-10 h-10 m-2 bg-${item}-500 border border-${item}-700 flex gap-5`}></button>
+          ))
+        }
+        </div>
+        }
+      </div>
+
+      <div></div>
     </div>
   );
 }
