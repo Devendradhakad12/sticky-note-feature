0a00834c5778cb19890cfe9ca3a8b1ef30a0896b
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-12-16 12:35:10 +0530

    dynamic font size added

--- src/components/ShowNote.jsx
+++ src/components/ShowNote.jsx
@@ -1,23 +1,22 @@
+"use client";
+
+import { useRef } from "react";
+
 // Show Note Component
-
-
-
 export const ShowNote = ({ note, currentNote, setCurrentNote, UpdateNote }) => {
-   
   const calculateFontSize = () => {
     const baseFontSize = 16;
     const scaleFactor = 0.9;
     const maxFontSize = 30;
 
-    const lines = note.text.split(' ').length;
+    const lines = note.text.split(" ").length;
     const calculatedFontSize = Math.min(
       maxFontSize,
       baseFontSize * scaleFactor ** lines
     );
-
     return `${calculatedFontSize}px`;
   };
-  
+
   return (
     <textarea
       // readOnly={currentNote != note.id ? true : false}
@@ -25,12 +24,14 @@ export const ShowNote = ({ note, currentNote, setCurrentNote, UpdateNote }) => {
       value={note?.text}
       onDoubleClick={() => setCurrentNote(note.id)}
       id={note.id}
-      style={{ left: note.x, top: note.y, backgroundColor: note.bgColor,fontSize: calculateFontSize()}}
+      style={{
+        left: note.x,
+        top: note.y,
+        backgroundColor: note.bgColor,
+        fontSize: calculateFontSize(),
+      }}
       className=" p-3 absolute read-only:cursor-default read-only:outline-none "
       onChange={(e) => UpdateNote(e)}
-       
     />
   );
 };
-
- 
